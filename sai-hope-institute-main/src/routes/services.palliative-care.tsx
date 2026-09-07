751138501cbbb5141dc3b19df3aad8307f1d5899
sumirayandesign-netizen <sumirayandesign@gmail.com>
2026-09-07 17:29:32 +0530

    Update services.palliative-care.tsx

--- sai-hope-institute-main/src/routes/services.palliative-care.tsx
+++ sai-hope-institute-main/src/routes/services.palliative-care.tsx
@@ -7,7 +7,7 @@ export const Route = createFileRoute('/services/palliative-care')({
 function ComingSoonPage() {
   return (
     <div className="py-32 text-center">
-      <h1 className="text-4xl font-bold text-deep">Medical Oncology</h1>
+      <h1 className="text-4xl font-bold text-deep">palliative-care</h1>
       <p className="mt-4 text-gray-500 text-lg">Detailed information is coming soon...</p>
     </div>
   )
